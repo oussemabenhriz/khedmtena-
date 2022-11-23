c83af0744a7ec569ebf71f0f3020db0df614d18a
oussemabenhriz <oussemabenhariz33@gmail.com>
2022-11-23 17:57:42 +0100

    js dom

---
+++ main.js
@@ -0,0 +1,35 @@
+let btndelete =document.querySelectorAll(`.btn-delete`);
+for (let i=0; i<btndelete.length; i++){
+    btndelete[i].addEventListener(`click`,function(){
+        btndelete[i].parentElement.parentElement.remove()
+    })
+
+}
+
+let bouttonplus =document.getElementsByClassName(`btn-plus`);
+for(let i=0; i<bouttonplus.length; i++){
+    bouttonplus[i].addEventListener('click',function(){
+        bouttonplus[i].previousElementSibling.innerText ++;
+    });
+}
+let bouttonmoins =document.getElementsByClassName('btn-moins');
+for(let i=0 ; i<bouttonmoins.length; i++){
+    bouttonmoins[i].addEventListener('click',function(){
+        if( bouttonmoins[i].nextElementSibling.innerText > 0)
+            bouttonmoins[i].nextElementSibling.innerText -- ;
+        
+    })
+    }
+
+    let heart =document.getElementsByClassName('fas fa-heart');
+    for(let i=0 ;i<heart.length; i++){
+        heart[i].addEventListener('click',function(){
+            if(heart[i].style.color=== 'grey'){
+            heart[i].style.color='red'}
+            else{heart[i].style.color='grey'}
+        });
+    
+    }
+
+   
+
